c6bdecec546a99448b9da246a46864a637e75df3
dulumary <lecture@hagulu.com>
2025-08-29 20:22:16 +0900

    [김인규]  - mybatis insert  예제

--- src/main/java/com/marondal/springexample/mybatis/repository/ReviewRepository.java
+++ src/main/java/com/marondal/springexample/mybatis/repository/ReviewRepository.java
@@ -10,6 +10,15 @@ public interface ReviewRepository {
     // 전달은 id와 일치하는 new_review 테이블의 행 조회
     public Review selectReview(@Param("id") int id);
 
+    // review 테이블에 한행을 저장하는 기능
+    public int insertReview(
+            @Param("storeId") int storeId
+            , @Param("menu") String menu
+            , @Param("userName") String userName
+            , @Param("point") double point
+            , @Param("review") String review);
 
+    // Review 객체를 전달해서 new_review에 저장하는 기능
+    public int insertReviewByObject(Review review);
 
 }
--- src/main/java/com/marondal/springexample/mybatis/service/ReviewService.java
+++ src/main/java/com/marondal/springexample/mybatis/service/ReviewService.java
@@ -19,6 +19,28 @@ public class ReviewService {
         return review;
     }
 
+    // 리뷰정보를 전달 받고, 리뷰를 추가하는 기능
+    public int createReview(
+            int storeId
+            , String menu
+            , String userName
+            , double point
+            , String review) {
+
+        // review 테이블에 한행을 저장 하는 기능
+        int count = reviewRepository.insertReview(storeId, menu, userName, point, review);
+        return count;
+    }
+
+    // 리뷰정보를 객체로 전달받고 저장하는 기능
+    public int createReviewByObject(Review review) {
+
+        // Review 객체를 전달해서 new_review에 저장하는 기능
+        int count = reviewRepository.insertReviewByObject(review);
+
+        return count;
+    }
+
 
 
 }
